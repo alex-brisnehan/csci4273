96148c040fa267e401bbee9053877b5d6384f273
alex-brisnehan <alexander.brisnehan@colorado.edu>
2014-10-29 18:54:25 -0600

    Added in the other header files

--- pa3/EventScheduler.h
+++ pa3/EventScheduler.h
@@ -5,3 +5,22 @@
  * Use: The header file for the EventScheduler class*
  ****************************************************/
 
+#ifndef _EVENTSCHEDULER_
+#define _EVENTSCHEDULER_
+
+#include <list>
+#include <string.h>
+
+using namespace std;
+
+class EventScheduler {
+    public:
+	EventScheduler(size_t maxEvents);
+	~EventScheduer();
+	int eventSchedule(void evFunction(void *), void * arg, int timeout);
+	void eventCancel(int eventId);
+    private:
+	int eventId;
+}
+
+#endif
--- pa3/ThreadPool.h
+++ pa3/ThreadPool.h
@@ -5,3 +5,21 @@
  * Use: The header file for the ThreadPool class*
  ************************************************/
 
+#ifndef _THREADPOOL_
+#define _THREADPOOL_
+
+#include <pthread.h>
+#include <string.h>
+
+using namespace std;
+
+class ThreadPool {
+    public:
+	ThreadPool(size_t threadCount);
+	~ThreadPool();
+	int dispatch_thread(void dispatch_function(void*), void *arg);
+	bool thread_avail();
+};
+
+#endif
+
